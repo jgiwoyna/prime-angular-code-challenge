0ebda6bd1c5d9662a1cbd95bbede310ba3d32fdb
Joseph Giwoyna <josephgiwoyna@Josephs-MacBook-Pro-2.local>
2016-12-01 17:15:30 -0600

    updated controllers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -22,6 +22,37 @@ app.controller('HeroesController', ['$http', function($http) {
   console.log('heroes controller running');
   var self = this;
   self.heroes = {};
+
+  // function getHeroes(){
+  //   $http.get('/heroes')
+  //     .then(function(res) {
+  //       self.heroes = res.data;
+  //       console.log('self.heroes', self.heroes);
+  //     })
+  //   }
+
+  self.addHero = function() {
+  console.log('new hero: ', self.newHero);
+  $http.post('/heroes', self.newHero)
+    .then(function(response) {
+      console.log('POST finished.');
+      self.newHero = {};
+    });
+}
+
+// self.removeHero = function(id) {
+//   $http.delete('/heroes/' + id)
+//     .then(function(response) {
+//       getHeroes();
+//     });
+// }
+
+}]);
+
+app.controller('PowersController', ['$http', function($http) {
+  console.log('powers controller running');
+  var self = this;
+  self.heroes = {};
   getHeroes();
 
   function getHeroes(){
@@ -32,35 +63,12 @@ app.controller('HeroesController', ['$http', function($http) {
       })
     }
 
-  self.addHero = function() {
-  console.log('new hero: ', self.newHero);
-  $http.post('/heroes', self.newHero)
-    .then(function(response) {
-      console.log('POST finished.');
-      self.newHero = {};
-      getHeroes();
-    });
-}
+    self.removeHero = function(id) {
+      $http.delete('/heroes/' + id)
+        .then(function(response) {
+          getHeroes();
+        });
+    }
 
-self.removeHero = function(id) {
-  $http.delete('/heroes/' + id)
-    .then(function(response) {
-      getHeroes();
-    });
-}
 
-}]);
-
-app.controller('PowersController', ['$http', function($http) {
-  console.log('powers controller running');
-  var self = this;
-  self.pc = {};
-
-// function getPowers(){
-//   $http.get('/superpowers')
-//     .then(function(res) {
-//       self.pc = res.data;
-//       console.log('self.pc', self.pc);
-//     })
-//   }
 }]);
